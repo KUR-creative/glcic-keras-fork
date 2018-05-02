afcc17dc93cfd04e7fa9867a9ed1cd63393856d6
kur <rhdnfka94@gmail.com>
2018-05-02 22:11:15 +0900

    refactor: extract reusable functions from test.py

--- test.py
+++ test.py
@@ -6,54 +6,50 @@ from keras.layers import Input, Add, Multiply, merge
 from keras.models import Model
 from keras.utils import plot_model
 
-VAR_IMG_SHAPE = (None,None,3)
-VAR_MASK_SHAPE = (None,None,1)
+def load_compl_model(model_path, img_shape=(None,None,3)):
+    complnet_inp = Input(shape=img_shape, name='complnet_inp')
+    complnet_out = completion_net(img_shape)(complnet_inp)
+    compl_model = Model([complnet_inp], complnet_out)
+    compl_model.load_weights(model_path, by_name=True)
 
-complnet_inp = Input(shape=VAR_IMG_SHAPE, name='complnet_inp')
-masked_origins_inp = Input(shape=VAR_IMG_SHAPE, name='masked_origins_inp')
-masks_inp = Input(shape=VAR_MASK_SHAPE, name='masks_inp')
+    compl_model.summary()
+    plot_model(compl_model, to_file='C_model_test.png', 
+               show_shapes=True)
+    return compl_model
 
-complnet_out = completion_net(VAR_IMG_SHAPE)(complnet_inp)
-merged_out = Add()([masked_origins_inp, 
-                     Multiply()([complnet_out, 
-                                 masks_inp])])
-compl_model = Model([masked_origins_inp, 
-                     complnet_inp, 
-                     masks_inp], merged_out)
-compl_model.load_weights('./output/complnet_12.h5',by_name=True)
-#                    TODO:hoxy... 
+def load_image(img_path):
+    origin = cv2.imread(img_path)
+    origin = cv2.cvtColor(origin,cv2.COLOR_BGR2RGB)
+    origin = origin.astype(np.float32) / 255
+    return origin, origin.shape[:2]
 
-compl_model.summary()
-plot_model(compl_model, to_file='C_model_test.png', show_shapes=True)
+def mask_from_user(mask_hw,mean_pixel_val):
+    h,w = mask_hw
+    mean_mask = np.zeros((h,w,1), dtype=np.float32)
+    mean_mask[199:378,520:690] = mean_pixel_val # hwkqrxnfr8vy
+    #mask[60:100,30:130] = 1.0 # other
+    return mean_mask, np.logical_not(mean_mask).astype(np.float32)
 
+origin, hw = load_image('./data/test_images/1099100.png')
+mean_mask, not_mask = mask_from_user(hw, np.mean(origin))
+holed_origin = origin * not_mask
+complnet_input = np.copy(holed_origin) + mean_mask
 
-origin = cv2.imread('./data/test_images/hwkqrxnfr8vy.jpg')
-#origin = cv2.imread('./data/test_images/download.jpg')
-#origin = cv2.imread('./data/test_images/149_0.png')
-#origin = cv2.imread('./data/test_images/big_square.jpg')
-origin = cv2.cvtColor(origin,cv2.COLOR_BGR2RGB)
-origin = origin.astype(np.float32) / 255
-hw = origin.shape[:2]
-print(hw)
-
-# mask
+compl_model = load_compl_model('./output/old/complnet_199.h5')
 h,w = hw
-mask = np.zeros((h,w,1), dtype=np.float32)
-#mask[199:378,520:690] = 1.0 # hwkqrxnfr8vy
-mask[10:50,20:60] = 1.0 # other
+complnet_output = compl_model.predict(
+                    [complnet_input.reshape((1,h,w,3))]
+                  )
 
-# masked origin
-not_mask = np.logical_not(mask).astype(np.float32)
-masked_origin = origin * not_mask
-
-# complnet input
-complnet_input = np.copy(masked_origin)
+print(origin.shape)
+print(complnet_output.shape)
 #cv2.imshow('input',origin); cv2.waitKey(0)
-#cv2.imshow('not_mask',not_mask); cv2.waitKey(0)
-#cv2.imshow('masked_origin',masked_origin); cv2.waitKey(0)
-#cv2.imshow('complnet_input',complnet_input); cv2.waitKey(0)
-
-completed = compl_model.predict([masked_origin.reshape((1,h,w,3)), 
-                                 complnet_input.reshape((1,h,w,3)), 
-                                 mask.reshape((1,h,w,1))])
-cv2.imshow('completed',completed.reshape((h,w,3))); cv2.waitKey(0)
+cv2.imshow('origin',origin); cv2.waitKey(0)
+cv2.imshow('mean_mask',mean_mask); cv2.waitKey(0)
+cv2.imshow('not_mask',not_mask); cv2.waitKey(0)
+cv2.imshow('holed_origin',holed_origin); cv2.waitKey(0)
+cv2.imshow('complnet_input',complnet_input); cv2.waitKey(0)
+cv2.imshow('complnet_output',
+           complnet_output.reshape((complnet_output.shape[1],
+                                    complnet_output.shape[2],
+                                    3))); cv2.waitKey(0)
