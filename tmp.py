74936ef1cac710760dd03a0968e26aaa8d153909
kur <rhdnfka94@gmail.com>
2018-05-03 13:59:08 +0900

    extract tester ui function(need to test)

--- tmp.py
+++ tmp.py
@@ -23,19 +23,23 @@ def draw_mask(event,x,y,flags,param):
         else:
             cv2.circle(mask,(x,y),5,color,-1)
 
-bg = cv2.imread('./data/test_images/images 2.jpg')
-mask = np.ones(bg.shape, np.float32)
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_mask)
-screen = np.empty(bg.shape)
-while(1):
-    screen = (bg.astype(np.float32) / 255) * mask 
-    cv2.imshow('image',screen)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 13: #ESC = 27, CarriageReturn(enter) = 13
-        break
-cv2.destroyAllWindows()
-cv2.imshow('mask',np.logical_not(mask).astype(np.float32)); cv2.waitKey(0)
+def tester_ui(mean_pixel_value):
+    bg = cv2.imread('./data/test_images/images 2.jpg')
+    mask = np.ones(bg.shape, np.float32)
+
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image',draw_mask)
+    screen = np.empty(bg.shape)
+    while(1):
+        screen = (bg.astype(np.float32) / 255) * mask 
+        cv2.imshow('image',screen)
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('m'):
+            mode = not mode
+        elif k == 13: #ESC = 27, CarriageReturn(enter) = 13
+            break
+    cv2.destroyAllWindows()
+    cv2.imshow('mask',np.logical_not(mask).astype(np.float32)); cv2.waitKey(0)
+
+tester_ui(100)
