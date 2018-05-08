bc728f9ce169832eb04affee84bb3b5d5d916f8d
kur <rhdnfka94@gmail.com>
2018-05-08 11:59:21 +0900

    itegrate test.py and tester_ui.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from networks import completion_net, discrimination_net
 from keras.layers import Input, Add, Multiply, merge
 from keras.models import Model
 from keras.utils import plot_model
+from tester_ui import tester_ui
 
 def load_compl_model(model_path, img_shape=(None,None,3)):
     complnet_inp = Input(shape=img_shape, name='complnet_inp')
@@ -25,13 +26,10 @@ def load_image(img_path):
 
 def mask_from_user(mask_hw, origin):
     h,w = mask_hw
-    mean_mask = np.zeros((h,w,1), dtype=np.float32)
-    #mean_mask[199:378,520:690] = np.mean(origin) # hwkqrxnfr8vy
-    #mean_mask[60:100,30:130] = np.mean(origin) # other
-    #mean_mask[105:136,70:102] = np.mean(origin) # test1
-    #mean_mask[343:437,225:327] = np.mean(origin) # test2
-    #mean_mask[750:972,456:677] = np.mean(origin) # test3
-    mean_mask[30:100,22:74] = np.mean(origin) # images 2
+    print('-------- ui start! --------')
+    bgr_origin = cv2.cvtColor(origin,cv2.COLOR_RGB2BGR)
+    mask = tester_ui(bgr_origin)
+    mean_mask = mask * np.mean(origin) # images 2
     return mean_mask, np.logical_not(mean_mask).astype(np.float32)
 
 def padding_removed(padded_img,no_pad_shape):
@@ -39,10 +37,6 @@ def padding_removed(padded_img,no_pad_shape):
     nH,nW,_ = no_pad_shape
     dH = pH - nH
     dW = pW - nW
-    #print('ps',padded_img.shape)
-    #print('ns',no_pad_shape)
-    #print('dH,dW',dH,dW)
-    #print('ret',padded_img[0:pH-dH,0:pW-dW].shape)
     # TODO: change this! it's temporary implementation!
     # TODO: 0~pH-dH is incorrect!
     return padded_img[0:pH-dH,0:pW-dW]
@@ -69,13 +63,15 @@ mask = np.logical_not(not_mask).astype(np.float32)
 completed = complnet_output * mask + holed_origin
 
 
-cv2.imshow('origin',origin); cv2.waitKey(0)
-cv2.imshow('mean_mask',mean_mask); cv2.waitKey(0)
-cv2.imshow('not_mask',not_mask); cv2.waitKey(0)
-cv2.imshow('mask',mask); cv2.waitKey(0)
-cv2.imshow('holed_origin',holed_origin); cv2.waitKey(0)
-cv2.imshow('complnet_input',complnet_input); cv2.waitKey(0)
-cv2.imshow('complnet_output',complnet_output); cv2.waitKey(0)
-cv2.imshow('completed',completed); cv2.waitKey(0)
+#bgr_origin = cv2.cvtColor(origin,cv2.COLOR_RGB2BGR)
+#cv2.imshow('origin',bgr_origin); cv2.waitKey(0)
+#cv2.imshow('mean_mask',mean_mask); cv2.waitKey(0)
+#cv2.imshow('not_mask',not_mask); cv2.waitKey(0)
+#cv2.imshow('mask',mask); cv2.waitKey(0)
+#cv2.imshow('holed_origin',holed_origin); cv2.waitKey(0)
+#cv2.imshow('complnet_input',complnet_input); cv2.waitKey(0)
+#cv2.imshow('complnet_output',complnet_output); cv2.waitKey(0)
+bgr_completed = cv2.cvtColor(completed,cv2.COLOR_RGB2BGR)
+cv2.imshow('completed',bgr_completed); cv2.waitKey(0)
 
 print('is it ok?')
--- tester_ui.py
+++ tester_ui.py
@@ -24,25 +24,28 @@ def draw_mask(event,x,y,flags,param):
         else:
             cv2.circle(mask,(x,y),5,color,-1)
 
-def tester_ui(background, not_mask, mean_pixel_value):
+def tester_ui(bg): # bg is standardized image(img / 255)
+    global mode
+    not_mask = np.ones(bg.shape, np.float32)
+
     cv2.namedWindow('image')
     cv2.setMouseCallback('image',draw_mask,not_mask)
 
     screen = np.empty(bg.shape)
     while(1):
-        screen = (bg.astype(np.float32) / 255) * not_mask 
+        screen = bg.astype(np.float32) * not_mask 
         cv2.imshow('image',screen)
         k = cv2.waitKey(1) & 0xFF
         if k == ord('m'):
             mode = not mode
         elif k == 13: #ESC = 27, CarriageReturn(enter) = 13
             break
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
     mask = np.logical_not(not_mask).astype(np.float32)
     return mask
 
 if __name__ == '__main__':
-    bg = cv2.imread('./data/test_images/images 2.jpg')
-    mask = tester_ui(bg, np.ones(bg.shape, np.float32), 100)
+    bg_img = cv2.imread('./data/test_images/images 2.jpg') / 255
+    mask = tester_ui(bg_img) 
     cv2.imshow('mask',mask); cv2.waitKey(0)
